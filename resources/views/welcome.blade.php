25c9e52b6af117e395cf1e7d01a0fe1118a9700f
pine5274 <pine5274@gmail.com>
2020-09-23 16:17:24 +0900

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+<div id="app">
+    <v-app>
+        <example-component></example-component>
+    </v-app>
+</div>
+<!-- Scripts -->
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
